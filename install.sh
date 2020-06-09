9b8e65910cf5797ad7ec6300665fcc752f3fb3e3
Vincent Zhu <chauvinhtth13@gmail.com>
2020-06-09 15:51:18 +0700

    Update

--- install.sh
+++ install.sh
@@ -10,6 +10,6 @@ make build CONF=pgsagen
 cd ..
 make
 
-export PATH=$PWD/KMC/bin:$PATH
+export PATH=$PWD/KMC/bin/:$PATH
 export PATH=$PWD/bin/:$PATH
 export PATH=$PWD/PgSA/dist/pgsagen/GNU-Linux-x86/:$PATH
