2eda0c63453ce7a654acff71b64d383ae4067800
VincentZhu <40750050+chauvinhtth13@users.noreply.github.com>
2020-06-09 11:54:39 +0700

    Update install_env.sh

--- install_env.sh
+++ install_env.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 sudo apt-get install gcc g++ build-essential autoconf automake autoreconf pkg-config libtool
-
-wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
-bash Anaconda3-2020.02-Linux-x86_64.sh 
-rm Anaconda3-2020.02-Linux-x86_64.sh 
+if which python3 >/dev/null; then
+    echo exists
+else
+    wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
+    bash Anaconda3-2020.02-Linux-x86_64.sh 
+    rm Anaconda3-2020.02-Linux-x86_64.sh 
+fi
 
 sudo apt-get install yaggo emboss
 
