cf47d17acc00b8cff41e3a287271a49a5e2ae000
VincentZhu <40750050+chauvinhtth13@users.noreply.github.com>
2020-06-09 10:44:46 +0700

    Setup File Enviroment
    Create Enviroment To Using HG-CoLor

---
+++ install_env.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo apt-get install gcc g++ build-essential autoconf automake yaggo pkg-config libtool gettext blasr emboss
+
+wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
+bash Anaconda3-2020.02-Linux-x86_64.sh 
+rm Anaconda3-2020.02-Linux-x86_64.sh 
+
+sudo apt-get update
+
+wget https://github.com/gmarcais/Jellyfish/archive/v2.3.0.tar.gz
+tar -zxvf v2.3.0.tar.gz 
+rm v2.3.0.tar.gz 
+cd Jellyfish-2.3.0/
+./configure
+autoreconf -i
+./configure
+make -j 4
+sudo make install
+pkg-config --exists jellyfish-2.0 && echo OK
+
+cd ..
+
+wget https://github.com/gmarcais/Quorum/releases/download/v1.1.1/quorum-1.1.1.tar.gz
+tar -zxvf quorum-1.1.1.tar.gz 
+rm quorum-1.1.1.tar.gz
+cd quorum-1.1.1/
+autoreconf -fi
+./configure
+make
+sudo make install
